739f1daf8e6d8dfe464d213fe31c4b188fb6845b
Olga Smith <ms.olgasmith.jj@mail.ru>
2023-08-09 21:26:56 +0300

    deploy again

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-export const hostURL = "http://api.moviepoisk.nomoredomains.xyz";
+export const hostURL = "https://api.moviepoisk.nomoredomains.xyz";
 //'http://localhost:3000/'
 // api.moviepoisk.nomoredomains.xyz
 export const moviesURL = "https://api.nomoreparties.co/beatfilm-movies";
